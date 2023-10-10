b698686517ff2a7fac3d73948681533d1f720c93
David <david.d.belenom@gmail.com>
2023-10-10 14:12:45 -0700

    Componente mostar-vacante livewire, mostar vacante en guest y menu hamburguesa funcionando

--- app/Models/Vacante.php
+++ app/Models/Vacante.php
@@ -22,4 +22,10 @@ class Vacante extends Model
         'imagen',
         'user_id'
     ];
+    public function categoria(){
+        return $this->belongsTo(Categoria::class);
+    }
+    public function salario(){
+        return $this->belongsTo(Salario::class);
+    }
 }
